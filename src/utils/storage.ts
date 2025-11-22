ce4f9340f5d2474467d58012c338cf04f5985789
duncsully <duncsully@gmail.com>
2025-11-21 17:52:28 -0800

    fix: remove unused file with type errors and fix type errors elsewhere

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,12 +1,12 @@
 export const storage = {
-    get<T>(key: string, defaultValue: T): T {
-        const value = localStorage.getItem(key);
-        return value ? JSON.parse(value) as T : defaultValue;
-    },
-    set<T>(key: string, value: T): void {
-        localStorage.setItem(key, JSON.stringify(value));
-    },
-    removeEventListener(key: string): void {
-        localStorage.removeItem(key);
-    }
-};
+  get<T>(key: string, defaultValue: T): T {
+    const value = localStorage.getItem(key)
+    return value ? (JSON.parse(value) as T) : defaultValue
+  },
+  set<T>(key: string, value: T): void {
+    localStorage.setItem(key, JSON.stringify(value))
+  },
+  remove(key: string): void {
+    localStorage.removeItem(key)
+  },
+}
