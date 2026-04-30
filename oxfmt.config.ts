1bedb6967055ba595d8a0f6b71deda81f8ac2767
duncsully <duncsully@gmail.com>
2026-04-30 15:58:48 -0700

    deps: add oxfmt config

---
+++ oxfmt.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'oxfmt'
+
+export default defineConfig({
+  bracketSpacing: true,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 80,
+})
